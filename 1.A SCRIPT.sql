d32c4640cd03375623df2221f9fdf2edcdeccf20
Tejasmreddy <30894722+Tejasmreddy@users.noreply.github.com>
2025-07-13 17:37:16 +0530

    Delete 1.A SCRIPT.sql

--- 1.A SCRIPT.sql
+++
@@ -1,5 +0,0 @@
--- a) Write a query to retrieve student details, such as student name, contact informations, and Enrollment status.
-use  Academic_Management_System; 
-SELECT S.STU_NAME, S.PHONE_NO, E.COURSE_ID, E.ENROLL_STATUS
-FROM STUDENTINFO S 
-JOIN enrollmentinfo E ON S.STU_ID=E.STU_ID
